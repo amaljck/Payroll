aeee5def589ce5806b522fd5f274fba531055b57
Amal Johnson <luffy@LuffY.luffy.kali>
2025-09-21 14:38:45 +0530

    Initial commit

---
+++ front-end/js/index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function revealOnScroll() {
+        const reveals = document.querySelectorAll('.reveal');
+        const windowHeight = window.innerHeight;
+        reveals.forEach(function(el) {
+            const elementTop = el.getBoundingClientRect().top;
+            if (elementTop < windowHeight - 60) {
+                el.classList.add('visible');
+            }
+        });
+    }
+    window.addEventListener('scroll', revealOnScroll);
+    revealOnScroll(); // Initial check
+})
